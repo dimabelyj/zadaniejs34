64be413adcbf48b35b66c7da7cb1a1e3fbb47f3e
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-03-29 20:07:26 +0100

    koniec

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let te = "Uwielbiam JavaScript";
+let tex = "Jestem świetnym programistą";
+
+function ar(te, tex) {
+    /*for (te > tex)
+    let lenth_tx = tx;
+    let lenth_tex = tex;
+    if */
+    //let length_te = te;
+    //let length_tex = tex;
+    if ("te" > "tex") {
+        console.log(te);
+    } else if ("te" < "tex") {
+        console.log(tex);
+        return ar;
+    }
+
+}
